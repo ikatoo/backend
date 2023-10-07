67d56cb897f2cff6805f4f5af5f1ec60521b7f04
Milton Carlos Katoo <mckatoo@gmail.com>
2023-10-06 23:41:10 -0300

    removed unnecessary modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,11 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './infra/db/prisma/prisma.module';
-import { CryptoModule } from './infra/security/crypto/crypto.module';
 import { UserModule } from './modules/user/user.module';
 import { AboutPageModule } from './modules/about-page/about-page.module';
 import { ContactPageModule } from './modules/contact-page/contact-page.module';
 import { ProjectsModule } from './modules/projects/projects.module';
 
 @Module({
-  imports: [
-    PrismaModule,
-    CryptoModule,
-    UserModule,
-    AboutPageModule,
-    ContactPageModule,
-    ProjectsModule,
-  ],
+  imports: [UserModule, AboutPageModule, ContactPageModule, ProjectsModule],
   controllers: [],
   providers: [],
 })
