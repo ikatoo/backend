0f120158596f337b20c46dd9cf556252d2f70685
Milton Carlos Katoo <mckatoo@gmail.com>
2023-10-23 13:42:10 -0300

    refactoring services to use pg

--- src/infra/db/pg/pg.service.ts
+++ src/infra/db/pg/pg.service.ts
@@ -4,13 +4,17 @@ import { env } from 'process';
 
 @Injectable()
 export class PgService {
-  async getClient() {
-    return new Client({
-      host: env.POSTGRES_HOSTNAME,
-      database: env.POSTGRES_DBNAME,
-      port: +env.POSTGRES_PORT,
-      password: env.POSTGRES_PASSWORD,
-      user: env.POSTGRES_USER,
-    });
-  }
+  client = new Client({
+    host: env.POSTGRES_HOSTNAME,
+    database: env.POSTGRES_DBNAME,
+    port: +env.POSTGRES_PORT,
+    password: env.POSTGRES_PASSWORD,
+    user: env.POSTGRES_USER,
+  });
+
+  connect = this.client.connect;
+
+  end = this.client.end;
+
+  query = this.client.query;
 }
