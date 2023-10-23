e16526b7380455251989d2bf5a31b72963455891
Milton Carlos Katoo <mckatoo@gmail.com>
2023-10-23 13:42:10 -0300

    refactoring services to use pg

--- src/infra/db/pg/user/user.service.ts
+++ src/infra/db/pg/user/user.service.ts
@@ -4,45 +4,38 @@ import { IUserService } from 'src/modules/user/IUserService';
 import { CreateUserDto } from 'src/modules/user/dto/create-user.dto';
 import { UpdateUserDto } from 'src/modules/user/dto/update-user.dto';
 import { PgService } from '../pg.service';
-import { Client } from 'pg';
 
 @Injectable()
 export class UserServicePg implements IUserService {
   constructor(
     private readonly db: PgService,
     private readonly crypto: CryptoService,
-    private pgClient: Client,
-  ) {
-    (async () => {
-      pgClient = await db.getClient();
-    })();
-  }
+  ) {}
 
   async listAll() {
-    await this.pgClient.connect();
-    const users = (
-      await this.pgClient.query('select id, name, email from users;')
-    ).rows;
-    await this.pgClient.end();
+    await this.db.connect();
+    const users = (await this.db.query('select id, name, email from users;'))
+      .rows;
+    await this.db.end();
 
     return users;
   }
 
   async create(createUserDto: CreateUserDto) {
-    await this.pgClient.connect();
+    await this.db.connect();
 
     const { password, name, email } = createUserDto;
     const hashPassword = await this.crypto.hasher(8, password);
-    await this.pgClient.query(
+    await this.db.query(
       'insert into users(name, email, hash_password) values ($1, $2, $3)',
       [name, email, hashPassword],
     );
 
-    await this.pgClient.end();
+    await this.db.end();
   }
 
   async update(id: number, updateUserDto: UpdateUserDto) {
-    await this.pgClient.connect();
+    await this.db.connect();
 
     const user = updateUserDto;
     if (!!user.password) {
@@ -52,20 +45,20 @@ export class UserServicePg implements IUserService {
     const fields = Object.keys(user).toString();
     const values = Object.values(user).toString();
 
-    await this.pgClient.query('update users set ($1) = ($2) where id = $3;', [
+    await this.db.query('update users set ($1) = ($2) where id = $3;', [
       fields,
       values,
       id,
     ]);
 
-    await this.pgClient.end();
+    await this.db.end();
   }
 
   async remove(id: number) {
     if (!id) return;
 
-    await this.pgClient.connect();
-    await this.pgClient.query('delete from users where id = $1', [id]);
-    await this.pgClient.end();
+    await this.db.connect();
+    await this.db.query('delete from users where id = $1', [id]);
+    await this.db.end();
   }
 }
--- src/modules/about-page/about-page.service.spec.ts
+++ src/modules/about-page/about-page.service.spec.ts
@@ -1,13 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PgService } from 'src/infra/db/pg/pg.service';
 import { AboutPageService } from './about-page.service';
-import { PrismaService } from '../../infra/db/prisma/prisma.service';
 
 describe('AboutPageService', () => {
   let service: AboutPageService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AboutPageService, PrismaService],
+      providers: [AboutPageService, PgService],
     }).compile();
 
     service = module.get<AboutPageService>(AboutPageService);
