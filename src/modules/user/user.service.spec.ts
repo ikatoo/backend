3777212aab0b4356f5bf9587b6cf4d79ea921663
Milton Carlos Katoo <mckatoo@gmail.com>
2023-10-25 17:56:24 -0300

    added constructor on pg_promise_service

--- src/modules/user/user.service.spec.ts
+++ src/modules/user/user.service.spec.ts
@@ -4,29 +4,27 @@ import { CryptoService } from 'src/infra/security/crypto/crypto.service';
 import { UsersService } from './user.service';
 
 describe('UserService', () => {
-  let service: UsersService;
-  // let pgp: PgPromiseService;
+  let userService: UsersService;
+  let pgp: PgPromiseService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [UsersService, CryptoService, PgPromiseService],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
-    // pgp = module.get<PgPromiseService>(PgPromiseService);
+    userService = module.get<UsersService>(UsersService);
+    pgp = module.get<PgPromiseService>(PgPromiseService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
   });
 
   it('should create a user without error', async () => {
-    await expect(
-      service.create({
-        name: 'Tetste',
-        email: 'asdf@asdf.com',
-        password: 'asldfjasdf',
-      }),
-    ).resolves.not.toThrowError();
+    await userService.create({
+      name: 'Tetste',
+      email: 'asdf@asdf.com',
+      password: 'asldfjasdf',
+    });
   });
 });
