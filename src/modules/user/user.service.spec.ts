4c8b1ce256c03a5f5e45335e258669e81c393c67
Milton Carlos Katoo <mckatoo@gmail.com>
2023-10-25 20:23:46 -0300

    update user with test

--- src/modules/user/user.service.spec.ts
+++ src/modules/user/user.service.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { randomBytes } from 'crypto';
+import { IDatabase } from 'pg-promise';
 import { PgPromiseService } from 'src/infra/db/pg-promise/pg-promise.service';
 import { CryptoService } from 'src/infra/security/crypto/crypto.service';
 import { UsersService } from './user.service';
@@ -6,6 +8,7 @@ import { UsersService } from './user.service';
 describe('UserService', () => {
   let userService: UsersService;
   let pgp: PgPromiseService;
+  let db: IDatabase<any>;
   const crypto = new CryptoService();
 
   beforeEach(async () => {
@@ -15,7 +18,8 @@ describe('UserService', () => {
 
     userService = module.get<UsersService>(UsersService);
     pgp = module.get<PgPromiseService>(PgPromiseService);
-    pgp.db.none('delete from users;');
+    db = pgp.db;
+    db.none('delete from users;');
   });
 
   it('should be defined', () => {
@@ -39,5 +43,25 @@ describe('UserService', () => {
     expect(createdUser).toEqual(expected);
     expect(await crypto.compareHash(password, hash_password)).toBeTruthy();
     expect(id).toBeGreaterThan(0);
+  });
+
+  it('shoud update user', async () => {
+    const testId = randomBytes(4).toString('hex');
+    const mockedUser = {
+      name: 'Tetste',
+      email: 'asdf@asdf.com',
+      password: 'asldfjasdf',
+    };
+    const newValues = { name: '_new_name', password: '_new_password' };
+    const { id } = await db.oneOrNone(
+      'insert into users values(default, $1:raw) returning id',
+      [
+        Object.values(mockedUser)
+          .map((value) => `'${testId + value}'`)
+          .toString(),
+      ],
+    );
+
+    await userService.update(id, newValues);
   });
 });
