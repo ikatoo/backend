53ff14a165a7932d3e6c66eafae256ca74c4b2fe
Milton Carlos Katoo <mckatoo@gmail.com>
2023-11-01 20:06:08 -0300

    renamed contact and skill pages tables

--- src/modules/about-page/about-page.service.spec.ts
+++ src/modules/about-page/about-page.service.spec.ts
@@ -16,4 +16,16 @@ describe('AboutPageService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should get about page', () => {
+    const aboutPageMock = {
+      title: 'Olá. Bem vindo❗',
+      description:
+        '<p>Me chamo Milton Carlos Katoo, moro em Itapira, interior de São Paulo/Brasil. Pai de uma princesa e filho de excelente cozinheira Italiana e um saldoso Japonês faz tudo, sou um desenvolvedor full-stack que ama programação e desenvolvimento de software afim de melhorar a vida das pessoas.</p><p>Pessoa bem organizada, solucionador de problemas, funcionário independente com alta atenção aos detalhes. Gosto de animes, mangas, games, séries de TV e filmes. Pai orgulhoso de uma princesa, sou nascido em 1979 e sou innteressado em todo o espectro de programação.</p><p>🎉Vamos fazer algo especial.😄</p>',
+      image: {
+        url: '/public/teste.img',
+        alt: 'imagem de teste',
+      },
+    };
+  });
 });
