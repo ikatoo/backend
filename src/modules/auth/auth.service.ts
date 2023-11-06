ad4e9dd4f2d7f70d057421da66e5fa9660e3221a
Milton Carlos Katoo <mckatoo@gmail.com>
2023-11-06 10:56:16 -0300

    updated user using a new value

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PgPromiseService } from 'src/infra/db/pg-promise/pg-promise.service';
+import { CryptoService } from 'src/infra/security/crypto/crypto.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly pgp: PgPromiseService,
+    private readonly crypto: CryptoService,
+  ) {}
+
+  async signIn(email: string, password: string): Promise<any> {
+    const { hash_password, ...user } = await this.pgp.db.oneOrNone(
+      'select * from users where email=$1',
+      [email],
+    );
+    const authorized = await this.crypto.compareHash(password, hash_password);
+    if (authorized) {
+      throw new UnauthorizedException();
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    return user;
+  }
+}
