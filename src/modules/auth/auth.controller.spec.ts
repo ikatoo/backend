931d83c0c411aa3947dd0140e054ec83db75140f
Milton Carlos Katoo <mckatoo@gmail.com>
2023-11-06 21:51:28 -0300

    init auth module

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { CryptoService } from 'src/infra/security/crypto/crypto.service';
+import { PgPromiseService } from 'src/infra/db/pg-promise/pg-promise.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -7,6 +10,7 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [AuthService, CryptoService, PgPromiseService],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
