3a994298b458193da1a7e9b15bb27f073cb53386
Milton Carlos Katoo <mckatoo@gmail.com>
2023-11-10 01:16:21 -0300

    fix tests

--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { CryptoService } from 'src/infra/security/crypto/crypto.service';
 import { PgPromiseService } from 'src/infra/db/pg-promise/pg-promise.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PRIVATE_KEY } from 'src/constants';
 
 describe('AuthService', () => {
   let authService: AuthService;
@@ -10,6 +12,13 @@ describe('AuthService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        JwtModule.register({
+          global: true,
+          secret: PRIVATE_KEY,
+          signOptions: { expiresIn: '60s' },
+        }),
+      ],
       providers: [AuthService, CryptoService, PgPromiseService],
     }).compile();
 
