a7c22bafba0bbd300327c6f73b7dd6b402d5d3f7
Milton Carlos Katoo <mckatoo@gmail.com>
2023-11-11 21:32:56 -0300

    update init db script

--- src/infra/db/pg-promise/docker-entrypoint-initdb.d/init-user-db.sh
+++ src/infra/db/pg-promise/docker-entrypoint-initdb.d/init-user-db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -a -f src/infra/db/pg-promise/init.sql
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -a -f /db/scripts/init.sql
