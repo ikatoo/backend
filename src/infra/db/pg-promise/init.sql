89d51db83b688ebfe654cbf8e52050c80cb311b4
Milton Carlos Katoo <mckatoo@gmail.com>
2023-11-20 22:41:29 -0300

    skill only relates to user project

--- src/infra/db/pg-promise/init.sql
+++ src/infra/db/pg-promise/init.sql
@@ -36,8 +36,8 @@ CREATE TABLE "public"."projects" (
     "description" character varying NOT NULL,
     "snapshot" character varying NOT NULL,
     "repository_link" character varying NOT NULL,
-    "start" timestamp default now() NOT NULL,
-    "last_update" timestamp default now() NOT NULL,
+    "start" timestamp DEFAULT now() NOT NULL,
+    "last_update" timestamp DEFAULT now() NOT NULL,
     CONSTRAINT "projects_pkey" PRIMARY KEY ("id")
 ) WITH (oids = false);
 
@@ -62,23 +62,13 @@ CREATE TABLE "public"."skills" (
 ) WITH (oids = false);
 
 
-CREATE SEQUENCE skills_of_user_on_projects_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
+CREATE SEQUENCE skills_on_projects_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
 
-CREATE TABLE "public"."skills_of_user_on_projects" (
-    "id" integer DEFAULT nextval('skills_of_user_on_projects_id_seq') NOT NULL,
-    "skill_user_id" integer NOT NULL,
-    "project_user_id" integer NOT NULL,
-    CONSTRAINT "skills_of_user_on_projects_pkey" PRIMARY KEY ("id")
-) WITH (oids = false);
-
-
-CREATE SEQUENCE skills_on_users_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
-
-CREATE TABLE "public"."skills_on_users" (
-    "id" integer DEFAULT nextval('skills_on_users_id_seq') NOT NULL,
+CREATE TABLE "public"."skills_on_projects" (
+    "id" integer DEFAULT nextval('skills_on_projects_id_seq') NOT NULL,
     "skill_id" integer NOT NULL,
-    "user_id" integer NOT NULL,
-    CONSTRAINT "skills_on_users_pkey" PRIMARY KEY ("id")
+    "project_on_user_id" integer NOT NULL,
+    CONSTRAINT "skills_on_projects_pkey" PRIMARY KEY ("id")
 ) WITH (oids = false);
 
 
@@ -124,12 +114,9 @@ ALTER TABLE ONLY "public"."contact_pages" ADD CONSTRAINT "contacts_page_user_id_
 ALTER TABLE ONLY "public"."projects_on_users" ADD CONSTRAINT "projects_on_users_project_id_fkey" FOREIGN KEY (project_id) REFERENCES projects(id) ON UPDATE CASCADE ON DELETE CASCADE NOT DEFERRABLE;
 ALTER TABLE ONLY "public"."projects_on_users" ADD CONSTRAINT "projects_on_users_user_id_fkey" FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE NOT DEFERRABLE;
 
-ALTER TABLE ONLY "public"."skills_of_user_on_projects" ADD CONSTRAINT "skills_of_user_on_projects_project_id_fkey" FOREIGN KEY (project_user_id) REFERENCES projects_on_users(id) ON UPDATE CASCADE ON DELETE CASCADE NOT DEFERRABLE;
-ALTER TABLE ONLY "public"."skills_of_user_on_projects" ADD CONSTRAINT "skills_of_user_on_projects_skill_user_id_fkey" FOREIGN KEY (skill_user_id) REFERENCES skills_on_users(id) ON UPDATE CASCADE ON DELETE CASCADE NOT DEFERRABLE;
-
-ALTER TABLE ONLY "public"."skills_on_users" ADD CONSTRAINT "skills_on_users_skill_id_fkey" FOREIGN KEY (skill_id) REFERENCES skills(id) ON UPDATE CASCADE ON DELETE CASCADE NOT DEFERRABLE;
-ALTER TABLE ONLY "public"."skills_on_users" ADD CONSTRAINT "skills_on_users_user_id_fkey" FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE NOT DEFERRABLE;
+ALTER TABLE ONLY "public"."skills_on_projects" ADD CONSTRAINT "skills_on_projects_project_on_user_id_fkey" FOREIGN KEY (project_on_user_id) REFERENCES projects_on_users(id) ON UPDATE CASCADE ON DELETE CASCADE NOT DEFERRABLE;
+ALTER TABLE ONLY "public"."skills_on_projects" ADD CONSTRAINT "skills_on_projects_skill_id_fkey" FOREIGN KEY (skill_id) REFERENCES skills(id) ON UPDATE CASCADE ON DELETE CASCADE NOT DEFERRABLE;
 
 ALTER TABLE ONLY "public"."skills_pages" ADD CONSTRAINT "skills_page_user_id_fkey" FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE NOT DEFERRABLE;
 
--- 2023-11-17 13:44:38.744138+00
+-- 2023-11-21 01:37:51.026947+00
