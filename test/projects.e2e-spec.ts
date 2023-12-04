d79499a7dcd9ee4e41426895517192957529c7d2
Milton Carlos Katoo <mckatoo@gmail.com>
2023-12-04 00:55:43 -0300

    add project tests e2e

--- test/projects.e2e-spec.ts
+++ test/projects.e2e-spec.ts
@@ -3,11 +3,12 @@ import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { randomBytes, randomInt } from 'crypto';
 import { PgPromiseService } from 'src/infra/db/pg-promise/pg-promise.service';
-import { CreateProjectDto } from 'src/modules/projects/dto/create-project.dto';
+import { accessTokenFactory } from 'src/test-utils/access_token-factory';
+import { projectFactory } from 'src/test-utils/project-factory';
+import { projectOnUserFactory } from 'src/test-utils/project_on_user-factory';
+import { userFactory } from 'src/test-utils/user-factory';
 import request from 'supertest';
 import { AppModule } from '../src/app.module';
-import { accessTokenFactory } from 'src/test-utils/access_token-factory';
-import { userFactory } from 'src/test-utils/user-factory';
 
 describe('ProjectsController (e2e)', () => {
   let app: INestApplication;
@@ -68,20 +69,20 @@ describe('ProjectsController (e2e)', () => {
   it('/project (POST)', async () => {
     const randomTestId = randomBytes(3).toString('hex');
     const createdUser = await userFactory();
+    const start = new Date(`2021/${randomInt(1, 13)}/${randomInt(1, 28)}`);
+    const lastUpdate = new Date();
+    lastUpdate.setDate(start.getDate() + randomInt(500));
     const mockedData = {
-      title: `Project ${randomTestId}`,
-      description: `Description ${randomTestId}`,
-      snapshot: `Snapshot ${randomTestId}`,
-      repositoryLink: `RepositoryLink ${randomTestId}`,
-      start: new Date(`2021/${randomInt(1, 13)}/${randomInt(1, 28)}`),
-      lastUpdate: new Date(),
+      title: `title ${randomTestId}`,
+      description: `description ${randomTestId}`,
+      snapshot: `snapshot ${randomTestId}`,
+      repositoryLink: `repositoryLink ${randomTestId}`,
+      start,
+      lastUpdate,
+      skills: [{ title: `skill ${randomTestId}` }],
       userId: createdUser.id,
-      skills: [
-        { title: `Skill1 ${randomTestId}` },
-        { title: `Skill2 ${randomTestId}` },
-        { title: `Skill3 ${randomTestId}` },
-      ],
     };
+
     const token = await accessTokenFactory(
       createdUser.email,
       createdUser.password,
@@ -90,64 +91,87 @@ describe('ProjectsController (e2e)', () => {
       .post('/project')
       .set('Authorization', `Bearer ${token}`)
       .send(mockedData);
-    // const createdProject = await pgp.db.oneOrNone(
-    //   'select * from projects where title=$1;',
-    //   [mockedData.title],
-    // );
-    // console.log('createdProject ====>', createdProject);
-    // const expected = {
-    //   id: createdProject.id,
-    //   title: mockedData.title,
-    //   description: mockedData.description,
-    //   snapshot: mockedData.snapshot,
-    //   repository_link: mockedData.repositoryLink,
-    //   start: mockedData.start,
-    //   last_update: mockedData.lastUpdate,
-    // };
+
+    const createdProject = await pgp.db.oneOrNone(
+      'select * from projects where title=$1;',
+      [mockedData.title],
+    );
+
+    const expected = {
+      id: createdProject.id,
+      title: mockedData.title,
+      description: mockedData.description,
+      snapshot: mockedData.snapshot,
+      repository_link: mockedData.repositoryLink,
+      start: mockedData.start.toLocaleDateString(),
+      last_update: mockedData.lastUpdate.toLocaleDateString(),
+    };
 
     expect(body).toEqual({});
     expect(status).toEqual(201);
-    // expect(createdProject).toEqual(expected);
+    expect({
+      ...createdProject,
+      last_update: createdProject.last_update.toLocaleDateString(),
+      start: createdProject.start.toLocaleDateString(),
+    }).toEqual(expected);
   });
 
   it('/project/:id (PATCH)', async () => {
-    // await userFactory();
-    // const userMock = await userFactory();
-    // const newData = { name: 'Updated User' };
-    // const token = await accessTokenFactory(userMock.email, userMock.password);
-    // const { body, status } = await request(app.getHttpServer())
-    //   .patch(`/user`)
-    //   .set('Authorization', `Bearer ${token}`)
-    //   .send(newData);
-    // const { hash_password, ...updatedUser } = await pgp.db.oneOrNone(
-    //   'select id, name, email, hash_password from users where id=$1;',
-    //   [userMock.id],
-    // );
-    // const expected = {
-    //   id: userMock.id,
-    //   name: newData.name,
-    //   email: userMock.email,
-    // };
-    // expect(body).toEqual({});
-    // expect(status).toEqual(204);
-    // expect(updatedUser).toEqual(expected);
-    // expect(await compareHash(userMock.password, hash_password)).toBeTruthy();
+    const randomTestId = randomBytes(3).toString('hex');
+    const createdUser = await userFactory();
+    const createdProject = await projectFactory();
+    await projectOnUserFactory(createdProject.id, createdUser.id);
+    const newData = {
+      title: `New title ${randomTestId}`,
+      description: `New Desc ${randomTestId}`,
+    };
+
+    const token = await accessTokenFactory(
+      createdUser.email,
+      createdUser.password,
+    );
+    const { body, status } = await request(app.getHttpServer())
+      .patch(`/project/${createdProject.id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send(newData);
+
+    const updatedProject = await pgp.db.one(
+      'select * from projects where id=$1;',
+      [createdProject.id],
+    );
+    const expected = {
+      ...createdProject,
+      id: createdProject.id,
+      title: newData.title,
+      description: newData.description,
+    };
+
+    expect(body).toEqual({});
+    expect(status).toEqual(204);
+    expect(updatedProject).toEqual(expected);
   });
 
   it('/project/:id (DELETE)', async () => {
-    // await userFactory();
-    // const userMock = await userFactory();
-    // const token = await accessTokenFactory(userMock.email, userMock.password);
-    // const { body, status } = await request(app.getHttpServer())
-    //   .delete(`/user`)
-    //   .set('Authorization', `Bearer ${token}`)
-    //   .send();
-    // const deletedUser = await pgp.db.oneOrNone({
-    //   text: 'select name, email, hash_password from users where id=$1',
-    //   values: [userMock.id],
-    // });
-    // expect(status).toEqual(204);
-    // expect(body).toEqual({});
-    // expect(deletedUser).toBeNull();
+    const createdUser = await userFactory();
+    const createdProject = await projectFactory();
+    await projectOnUserFactory(createdProject.id, createdUser.id);
+
+    const token = await accessTokenFactory(
+      createdUser.email,
+      createdUser.password,
+    );
+    const { body, status } = await request(app.getHttpServer())
+      .delete(`/project/${createdProject.id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send();
+
+    const deletedProject = await pgp.db.oneOrNone(
+      'select * from projects where id=$1;',
+      [createdProject.id],
+    );
+
+    expect(status).toEqual(204);
+    expect(body).toEqual({});
+    expect(deletedProject).toBeNull();
   });
 });
