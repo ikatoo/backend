ff1eefe5ff6eb48bb568cea5c072ff4f87f599ea
Milton Carlos Katoo <mckatoo@gmail.com>
2023-12-27 00:06:04 -0300

    use test env when running tests

--- src/constants.ts
+++ src/constants.ts
@@ -1,15 +1,19 @@
 import 'dotenv/config';
 
+const NODE_ENV = process.env.NODE_ENV;
+
 export const PG_CONNECTION = 'PG_CONNECTION';
-export const PRIVATE_KEY = process.env.PRIVATE_KEY;
-export const REFRESH_PRIVATE_KEY = process.env.REFRESH_PRIVATE_KEY;
+export const PRIVATE_KEY = NODE_ENV ?? process.env.PRIVATE_KEY;
+export const REFRESH_PRIVATE_KEY = NODE_ENV ?? process.env.REFRESH_PRIVATE_KEY;
 export const ACCESS_TOKEN_EXPIRES_IN = '1h';
 export const REFRESH_TOKEN_EXPIRES_IN = '30d';
 
 export const CLOUDINARY = 'Cloudinary';
 
-export const CLOUDINARY_CLOUDNAME = process.env.CLOUDINARY_CLOUDNAME;
-export const CLOUDINARY_FOLDER = process.env.CLOUDINARY_FOLDER;
-export const CLOUDINARY_APIKEY = process.env.CLOUDINARY_APIKEY;
-export const CLOUDINARY_APISECRET = process.env.CLOUDINARY_APISECRET;
-export const CLOUDINARY_URL = process.env.CLOUDINARY_URL;
+export const CLOUDINARY_CLOUDNAME =
+  NODE_ENV ?? process.env.CLOUDINARY_CLOUDNAME;
+export const CLOUDINARY_FOLDER = NODE_ENV ?? process.env.CLOUDINARY_FOLDER;
+export const CLOUDINARY_APIKEY = NODE_ENV ?? process.env.CLOUDINARY_APIKEY;
+export const CLOUDINARY_APISECRET =
+  NODE_ENV ?? process.env.CLOUDINARY_APISECRET;
+export const CLOUDINARY_URL = NODE_ENV ?? process.env.CLOUDINARY_URL;
